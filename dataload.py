24368d4ac1523b799db6d3b3579d119d42f92f45
flotbot <57899625+albertkjoller@users.noreply.github.com>
2020-01-09 09:59:04 +0100

    Update dataload.py

--- dataload.py
+++ dataload.py
@@ -17,51 +17,6 @@ import pandas as pd
 
 CATEGORIES = ["Angry", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
 
-"""for category in CATEGORIES:  # do emotions
-    path = os.path.join(DATADIR,category)  # create path to emotions
-    for img in os.listdir(path):  # iterate over each image per emotion
-        img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
-        plt.imshow(img_array, cmap='gray')  # graph it
-        #plt.show()  # display!
-
-        break  # we just want one for now so break
-    break  #...and one more!
-    """
-
-#new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-
-
-"""training_data = []
-
-
-
-def create_training_data():
-    for category in CATEGORIES:  # do emotions
-
-        path = os.path.join(DATADIR,category)  # create path to emotions
-        class_num = CATEGORIES.index(category)  # get the classification  (0-6).
-
-        for img in tqdm(os.listdir(path)):  # iterate over each image per emotion
-            try:
-                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
-                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
-                training_data.append([new_array, class_num])  # add this to our training_data
-            except Exception as e:  # in the interest in keeping the output clean...
-                pass
-            #except OSError as e:
-            #    print("OSErrroBad img most likely", e, os.path.join(path,img))
-            #except Exception as e:
-            #    print("general exception", e, os.path.join(path,img))
-
-create_training_data()"""
-
-
-
-"""for features,label in training_data:
-    X.append(features)
-    y.append(label)"""
-
-
 filename = '/Users/AlbertoK/Desktop/fer2013.csv'
 data = pd.read_csv(filename, sep=",")
 data = data.drop('Usage',axis=1)
@@ -84,10 +39,6 @@ for i in range(len(pixels)):
     
 
 X = list(X)
-#y = np.array(data.emotion)
-
-
-
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 
