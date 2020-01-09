124e9c63c90a0274edfcfd0e6f1b67fd36817ac0
flotbot <albertkjoller@users.noreply.github.com>
2020-01-09 15:46:14 +0100

    Predictor live

--- predictor.py
+++ predictor.py
@@ -18,23 +18,100 @@ CATEGORIES = ["Angry", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
 #Evt. gøres på en anden måde med webcam eller andet
 def prepare(filepath):
     IMG_SIZE = 48  # 48 in txt-based
-    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+#    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(filepath, (IMG_SIZE, IMG_SIZE))
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+def __draw_label(img, text, pos, bg_color):
+    font_face = cv2.FONT_HERSHEY_SIMPLEX
+    scale = 0.4
+    color = (0, 0, 0)
+    thickness = cv2.FILLED
+    margin = 2
+
+    txt_size = cv2.getTextSize(text, font_face, scale, thickness)
+
+    end_x = pos[0] + txt_size[0][0] + margin
+    end_y = pos[1] - txt_size[0][1] - margin
+
+    cv2.rectangle(img, pos, (end_x, end_y), bg_color, thickness)
+    cv2.putText(img, text, pos, font_face, scale, color, 1, cv2.LINE_AA)
 
 
 model = tf.keras.models.load_model("64x3-CNN.model")
+"""
+camera = cv2.VideoCapture(0)
+return_value, image = camera.read()
+cv2.imwrite('opencv.png', image)
+del(camera)
 
-
-picture = '/Users/AlbertoK/Desktop/DTU/Januar 2020/emotionalligent/thumbnail_large.jpg'
+picture = 'opencv.png'
+#picture = '/Users/AlbertoK/Desktop/DTU/Januar2020/emotionalligent/thumbnail_large.jpg'
 
 prediction = model.predict([prepare(picture)])
-print(prediction)  # will be a list in a list.
+print(prediction)  # will be a list in a list."""
 
+key = cv2. waitKey(1)
+webcam = cv2.VideoCapture(0)
+
+while True:
+    try:
+        check, frame = webcam.read()
+        print(check) #prints true as long as the webcam is running
+        print(frame) #prints matrix values of each framecd 
+        cv2.imshow("Capturing", frame)
+        key = cv2.waitKey(1)
+        
+        prediction = model.predict([prepare(frame)])
+        print(prediction)  # will be a list in a list.
+        
+        text = CATEGORIES[np.where(prediction==np.max(prediction))[1][0]]
+        print(text)
+        
+        
+        font_face = cv2.FONT_HERSHEY_SIMPLEX
+        scale = 0.4
+        color = (255, 255, 255)
+        thickness = cv2.FILLED
+        margin = 2
+        pos = (20,20)
+    
+        txt_size = cv2.getTextSize(text, font_face, scale, thickness)
+    
+        end_x = pos[0] + txt_size[0][0] + margin
+        end_y = pos[1] - txt_size[0][1] - margin
+        
+        cv2.putText(frame,text,(end_x,end_y), font_face, scale,color,thickness,cv2.LINE_AA)
+       
+        
+        if key == ord('q'):
+            print("Turning off camera.")
+            webcam.release()
+            print("Camera off.")
+            print("Program ended.")
+            cv2.destroyAllWindows()
+            break
+        
+    except(KeyboardInterrupt):
+        print("Turning off camera.")
+        webcam.release()
+        print("Camera off.")
+        print("Program ended.")
+        cv2.destroyAllWindows()
+        break
+
+
+
+
+
+
+
+
+"""
 image = imread(picture)                                                                        
-plt.imshow(image)
-plt.axis('Off')
-print(CATEGORIES[np.where(prediction==np.max(prediction))[1][0]])
+cv2.imshow(image)
+#plt.axis('Off')
+print(CATEGORIES[np.where(prediction==np.max(prediction))[1][0]])"""
 
 
 
